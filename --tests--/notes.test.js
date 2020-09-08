c9d41533ac32ac35eef641467ff11ae8c6693e96
raghadanees <raghad.anees@hotmail.com>
2020-09-08 05:01:07 +0300

    lab2

---
+++ --tests--/notes.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Note = require('../lib/notes');
+
+// Spies!
+// we will get to know if something was called or not.
+jest.spyOn(global.console, 'log');
+
+describe('Note Module', () => {
+    // test case
+  it('execute() works', () => {
+    let newNoteObj = new Note({ payload: 'good note', action: '--a' });
+    newNoteObj.execute({ payload: 'good note', action: '--a' });
+    expect(console.log).toHaveBeenCalled();
+  });
+
+  it('add() does its work', () => {
+    let newNoteObj = new Note({ action: '--a', error: 'ERROR: this is not valid arg' });
+    newNoteObj.add({ action: '--a', error: 'ERROR: this is not valid arg' });
+    expect(console.log).toHaveBeenCalled();
+  });
+
+  it('add() does its work', () => {
+    let newNoteObj = new Note({ payload: 'good note', action: '--a', note: 'valid note' });
+    newNoteObj.add({ payload: 'good note', action: '--a', note: 'valid note' });
+    expect(console.log).toHaveBeenCalled();
+  });
+
+});
+
+// we did step on github (action) to add workflow of node
+// "test": "jest --verbose --coverage"   ضفناها داخل ملف (package.json)  
+//وبعدها عملنا npm test
